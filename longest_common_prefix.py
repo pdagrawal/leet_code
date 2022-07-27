5e4d636502d050cfd14d6bf15fdaf784dfcdf1a5
Pradeep Agrawal <pradeepagrawal79@gmail.com>
2022-07-27 11:01:49 -0400

    submitted longest_common_prefix program

--- longest_common_prefix.py
+++ longest_common_prefix.py
@@ -1,2 +1,18 @@
 def longest_common_prefix(strs):
-    pass
+    min_str = min(strs, key=len)
+    max_lim = len(min_str)
+    common = ''
+    should_add = True
+    for c in range(max_lim):
+        print(common)
+        for i in range(len(strs)):
+            if min_str[c] != strs[i][c]:
+                should_add = False
+        if should_add:
+            common += min_str[c]
+        else:
+            return common
+    return common
+
+print('Final: ', longest_common_prefix(["flower","flow","fl"]))
+print('Final: ', longest_common_prefix(["dog","racecar","car"]))
