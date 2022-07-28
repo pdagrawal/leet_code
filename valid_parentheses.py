d0de127195625613f7c2bf143d1668aa33ad444f
Pradeep Agrawal <pradeepagrawal79@gmail.com>
2022-07-28 10:41:29 -0400

    submitted valid_parentheses program

--- valid_parentheses.py
+++ valid_parentheses.py
@@ -1,2 +1,32 @@
 def valid_parentheses(s):
-    pass
+    if float(int(len(s)/2)) != len(s)/2:
+        return False
+
+    return remove_parentheses(s, 0)
+
+
+def remove_parentheses(st, i):
+    parentheses_mapping = {'(': ')', '{': '}', '[': ']'}
+    opening = '[{('
+    if i < 0:
+        i = 0
+    elif i >= len(st)/2:
+        return False
+    if st[i] in opening and st[i+1] == parentheses_mapping[st[i]]:
+        st = st[:i] + st[i + 2:]
+        if len(st) > 0:
+            return remove_parentheses(st, i - 1)
+        else:
+            return True
+    elif st[i] in opening:
+        return remove_parentheses(st, i + 1)
+    elif st[i] not in opening:
+        return False
+
+print(valid_parentheses('()()())'))
+print(valid_parentheses('()()()'))
+print(valid_parentheses('((()))'))
+print(valid_parentheses('((((((()'))
+print(valid_parentheses('(([]){{[()]}}))'))
+print(valid_parentheses('(([]{{[()]}}))'))
+print(valid_parentheses('(('))
