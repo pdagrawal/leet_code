d04721a33c0ee3112df438eec9a1e51f54cf1d5b
Pradeep Agrawal <pradeepagrawal79@gmail.com>
2022-07-31 12:53:08 -0400

    submitted fizz_buzz program

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -1,2 +1,17 @@
+from sre_parse import fix_flags
+
+
 def fizz_buzz(n):
-	pass
+	answer = []
+	for i in range(1, n+1):
+		if i % 3 == 0 and i % 5 == 0:
+			answer.append("FizzBuzz")
+		elif i % 3 == 0:
+			answer.append("Fizz")
+		elif i % 5 == 0:
+			answer.append("Buzz")
+		else:
+			answer.append(str(i))
+	return answer
+
+print(fizz_buzz(15))
