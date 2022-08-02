184e3dff73dc0fefec2132f0de642e51a30ae977
Pradeep Agrawal <pradeepagrawal79@gmail.com>
2022-08-01 21:37:36 -0400

    submitted middle_of_the_linked_list program

--- middle_of_the_linked_list.py
+++ middle_of_the_linked_list.py
@@ -1,2 +1,18 @@
-def middle_of_the_linked_list(head):
-	pass
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        start = head
+        count = 1
+        while head.next != None:
+            count += 1
+            head = head.next
+        middle_index = int((count if count % 2 == 0 else count - 1)/2)
+        for i in range(middle_index+1):
+            if i == middle_index:
+                return start
+            else:
+                start = start.next
