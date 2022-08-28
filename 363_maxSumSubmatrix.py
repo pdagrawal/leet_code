8f793f10e2c5403cf7e3b629af1901ba32e47f52
Pradeep Agrawal <pradeepagrawal79@gmail.com>
2022-08-28 00:36:47 -0400

    submitted maxSumSubmatrix program

---
+++ 363_maxSumSubmatrix.py
@@ -0,0 +1,23 @@
+def maxSumSubmatrix(matrix, k):
+    m, n = len(matrix), len(matrix[0])
+    res = -inf
+
+    for l in range(n):
+        rowSums = [0] * m
+        for r in range(l, n):
+            colSums = [0]
+            colSum = 0
+            for i in range(m):
+                rowSums[i] += matrix[i][r]
+                colSum += rowSums[i]
+                diff = colSum - k
+                idx = bisect_left(colSums, diff)
+                if idx < len(colSums):
+                    if colSums[idx] == diff:
+                        return k
+                    else:
+                        res = max(res, colSum - colSums[idx])
+                insort(colSums, colSum)
+    return res
+
+print(maxSumSubmatrix([[1,0,1],[0,-2,3]], 2))
