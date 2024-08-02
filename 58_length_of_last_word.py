26380bb2e21f7afd8a5447d8019976cf9a7ab6db
Pradeep Agrawal <pradeepagrawal79@gmail.com>
2024-08-01 22:52:41 -0500

    Added code files

---
+++ 58_length_of_last_word.py
@@ -0,0 +1,11 @@
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        words = s.strip().split()
+
+        if not words:
+            return 0
+        return len(words[-1])
+
+
+obj = Solution()
+print(obj.lengthOfLastWord("a"))
